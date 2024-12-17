5d72b00791257dd4360b530cd415e84f93a79f71
Jonathan Hesler <jeh8443@rit.edu>
2024-12-17 16:59:09 -0500

    Hardcoded view for question 1

--- CodeHistory/ContentView.swift
+++ CodeHistory/ContentView.swift
@@ -8,20 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     var body: some View {
-        VStack {
-            Text("Welcome to Code History!")
-                .font(.title)
-                .padding()
-            Button(action: {
-                print("Clicked")
-            }, label: {
-                Text("Click Me")
-            })
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(Color.white)
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1/10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding().multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                    }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                }
+            }
+            
         }
+        .foregroundColor(.white)
     }
 }
 
