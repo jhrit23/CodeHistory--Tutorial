4bcf9a4b1fa6915e2a370a868315d24910168ad4
Jonathan Hesler <jeh8443@rit.edu>
2024-12-17 15:54:29 -0500

    Initial Commit

---
+++ CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Jonathan Hesler on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeHistoryTests/CodeHistoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryTests.swift
+//  CodeHistoryTests
+//
+//  Created by Jonathan Hesler on 12/17/24.
+//
+
+import Testing
+@testable import CodeHistory
+
+struct CodeHistoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
