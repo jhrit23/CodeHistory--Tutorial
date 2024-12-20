0b83fd0fa610fc506ccffc04de36271078eaa840
Jonathan Hesler <jeh8443@rit.edu>
2024-12-20 18:04:05 -0500

    Question button ForEach and background changes color

--- CodeHistory/ContentView.swift
+++ CodeHistory/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     let question = Question(questionText: "What was the first computer bug?", possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"], correctAnswerIndex: 2)
     
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     
     var body: some View {
@@ -28,26 +28,14 @@ struct ContentView: View {
                     .padding().multilineTextAlignment(.leading)
                 Spacer()
                 HStack {
+                  ForEach(0..<question.possibleAnswers.count) { answerIndex in
                     Button(action: {
-                        print("Tapped on Choice 1")
+                      print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                        mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
                     }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[0])
+                      ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
                     })
-                    Button(action: {
-                        print("Tapped on Choice 2")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[1])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 3")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[2])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 4")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[3])
-                    })
+                  }
                 }
             }
             
